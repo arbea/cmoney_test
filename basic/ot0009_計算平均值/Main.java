b1fb8bbdd323b2e6dd3460279d37f515dbe85f15
SC-47 <max1839757@gmail.com>
2020-12-24 23:18:24 +0800

    Second Edition

--- basic/ot0009_計算平均值/Main.java
+++ basic/ot0009_計算平均值/Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int count = 1;
-    int sum = sc.nextInt();
-    while (sc.nextInt() != -1){
-      sum = sum+sc.nextInt();
+    int count = 0;
+    int input = sc.nextInt();
+    int sum = 0;
+    while (input != -1){
+      sum = sum+input;
       count += 1;
+      input = sc.nextInt();
     }
     System.out.print(sum/count);
     sc.close();
